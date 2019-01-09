283c8532a2f4fc39b4e8fbc881e0371fe5fcd98a
Reinier de Ridder <reinierdr@gmail.com>
2019-01-09 13:06:36 +0100

    Game.py documented, Feedback screen added after second error, code cleanup

--- Game.py
+++ Game.py
@@ -3,28 +3,22 @@ import sys
 from time import time
 import random
 import numpy
-from pygame.locals import *
-from pygame.compat import unichr_, unicode_
 
 from View import View
 from Board import Board
 
-''' This class is takes care of all the game logic and will be used as the Controller.  '''
+""" This class is takes care of all the game logic and will be used as the Controller.  """
+
 
 class Game:
-
-    ''' init Board + Player
-        Start view
-        Play game (while loop)
-        Communication between View and Model
-
-        Sequence randomizer (could be in Util) '''
-
+    """Initialize game settings"""
     SCREEN_SIZE = (1000, 800)
     BOARD_SIZE = (1000, 700)
     TILE_SIZE = (99, 99)
     SEQUENCE_LENGTH = 9
+    ALLOWED_ERRORS = 1
 
+    """Initialize needed global variables"""
     board = None
     correct_seq = None
     sequence = None
@@ -35,151 +29,239 @@ class Game:
     time_trial = None
     time_start = None
     SeqLength_Times_List = []
+    view = None
 
-    pygame.init()
-    pygame.display.set_mode(SCREEN_SIZE)
-    pygame.display.set_caption("Corsi Block Tapping Task")
+    """Create PyGame screen and initialize View.py to control the screen"""
 
-    screen = pygame.display.get_surface()
-    view = View(screen, SCREEN_SIZE, TILE_SIZE);
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_mode(self.SCREEN_SIZE)
+        pygame.display.set_caption("Corsi Block Tapping Task")
 
-    def initializeGame(self, CURRENT_SEQUENCELENGTH):
+        screen = pygame.display.get_surface()
+        self.view = View(screen, self.SCREEN_SIZE, self.TILE_SIZE)
+
+    """initialize Trial of length of current sequence"""
+
+    def initializeTrial(self, CURRENT_SEQUENCELENGTH):
+        """"Clean clickedseq by assigning new clean list()"""
         self.clickedseq = list()
-        self.board = Board(self.BOARD_SIZE, self.SEQUENCE_LENGTH, self.TILE_SIZE);
-        self.createSequence(CURRENT_SEQUENCELENGTH)  
+        """Clean board by creating new Board()"""
+        self.board = Board(self.BOARD_SIZE, self.SEQUENCE_LENGTH, self.TILE_SIZE)
+        """Create a new sequence of length of current sequence"""
+        self.createSequence(CURRENT_SEQUENCELENGTH)
+        """Tell View.py to draw the trial and show the sequence"""
         self.view.draw_trial(self.board.getCopy(), self.clickedseq)
         self.view.draw_sequence(self.getSequence())
+        """Set correct_seq to False to indicate player has not yet entered the correct sequence"""
         self.correct_seq = False
 
+    """Create a sequence of length of given variable CURRENT_SEQUENCELENGTH"""
+
     def createSequence(self, CURRENT_SEQUENCELENGTH):
-        i = CURRENT_SEQUENCELENGTH;
-        tiles = self.board.getCopy();
+        """Get copy of the tiles in the board from Board.py"""
+        tiles = self.board.getCopy()
+        """Shuffle them to always select a random tile"""
+        random.shuffle(tiles)
 
-        random.shuffle(tiles);
-
+        """Clean sequence by assigning new clean list()"""
         self.sequence = list()
 
-        for j in range(i):
+        """Add CURRENT_SEQUENCELENGTH amount of tiles from randomized tiles list to sequence"""
+        for j in range(CURRENT_SEQUENCELENGTH):
             self.sequence.append(tiles[j])
+
+    """Getter method that returns sequence list"""
 
     def getSequence(self):
         return self.sequence
-    
-    def button_pressed (self, msg, x, y, w, h):
+
+    """Check if button on location (x,y) with width w and height h is pressed"""
+
+    def button_pressed(self, x, y, w, h):
+        """Create clicked boolean and set to False"""
         clicked = False
+        """Get mouse pointer position and pressed mouse buttons"""
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
-        if x+w > mouse [0] > x and y+h > mouse[1] > y:
-            if click [0] == 1:
-               clicked = True
+
+        """Check if the mouse cursor is on the button"""
+        if x + w > mouse[0] > x and y + h > mouse[1] > y:
+            """Check if left mouse button had been pressed"""
+            if click[0] == 1:
+                """If so, set clicked to True"""
+                clicked = True
+
         return clicked
-    
-    def saveResults (self, results):
+
+    """Save the results of the Game to CSV file"""
+
+    # TODO IMRPOVE
+    def saveResults(self, results):
         filename = "CorsiBlockTapping.csv"
         myarray = numpy.asarray(results)
-        numpy.savetxt(filename, myarray, delimiter = ',')
-    
-    def avg (self, a):
-        del a[0]
-        sumList = sum(a)
-        print (sumList)
-        print (len(a))
-        avg = 500
-        return avg
-    
- 
+        numpy.savetxt(filename, myarray, delimiter=',')
+
+    """Main game loop to handle game logic"""
+
     def gameLoop(self):
+        """Set STATE to welcome"""
         STATE = "welcome"
+        """ Set CURRENT_SEQUENCELENGTH to 2"""
         CURRENT_SEQUENCELENGTH = 2
-        while(True):
 
-            self.view.refreshSurface();
+        """Start main game loop"""
+        while True:
 
+            """Clean screen from previous loop"""
+            self.view.refreshSurface()
+
+            """Get event list from pygame and do for every event in list:"""
             for event in pygame.event.get():
 
-                # interactive transitionals
-                if STATE == "welcome":
-                    if self.button_pressed("Start", 200, 450, 150, 75):
-                        self.initializeGame(CURRENT_SEQUENCELENGTH)
-                        self.time_start = time()
-                        STATE = "trial"
-                        continue
-                    if self.button_pressed("Quit", 600, 450, 150, 75):
-                        STATE = "quit"
-                        continue
+                """Transitionals"""
 
-                if STATE == "trial":
-                    if pygame.mouse.get_pressed() == (1,0,0):
-                        mouse_loc = pygame.mouse.get_pos()
-                        clickedRect = self.board.checkMouseClick(mouse_loc)
-                        if clickedRect != None:
-                            if (clickedRect.left, clickedRect.top) == self.sequence[len(self.clickedseq)]:
-                                self.clickedseq.append((clickedRect.left, clickedRect.top))
-                            else:
-                                self.time_trial = time() - self.time_start
-                                self.times.append(self.time_trial)
-                                self.SeqLength_Times_List.append((CURRENT_SEQUENCELENGTH-1, self.time_trial))
-                                self.player_errors += 1
-                                if self.player_errors > 1:
-                                    STATE = "final"
-                                    continue
-                                else:
-                                    STATE = "feedback"
-                                    continue
-                                
-                    if self.clickedseq == self.sequence:
-                            self.correct_seq = True
-                            CURRENT_SEQUENCELENGTH += 1
-                            self.player_errors = 0
-                            self.time_trial = time() - self.time_start
-                            self.times.append(self.time_trial)
-                            self.SeqLength_Times_List.append((CURRENT_SEQUENCELENGTH-1, self.time_trial))
-                            STATE = "feedback"
-        
-                    
-                        
-                if STATE == "feedback":
-                    if self.button_pressed('Next trial', 200, 450, 150, 75):                    
-                        if CURRENT_SEQUENCELENGTH <= self.SEQUENCE_LENGTH:
-                            self.initializeGame(CURRENT_SEQUENCELENGTH)
-                            self.time_start = time()
-                            STATE = "trial"
-                        else: 
-                            STATE = "final"
-                    if self.button_pressed('Quit', 600, 450, 150, 75):
-                        STATE = "final"
-                        
-                if STATE == "final":
-                    self.saveResults(self.SeqLength_Times_List)
-                    if self.button_pressed('Quit', 400, 450, 200, 100):
+                """If in welcome state"""
+                if STATE == "welcome":
+                    """If start trial button is pressed"""
+                    if self.button_pressed(200, 450, 150, 75):
+                        """Initialize new trial with CURRENT_SEQUENCELENGTH"""
+                        self.initializeTrial(CURRENT_SEQUENCELENGTH)
+                        """Set starttime of current trial to current time"""
+                        self.time_start = time()
+                        """Set STATE to trial"""
+                        STATE = "trial"
+                        """Go to next event in event list"""
+                        continue
+                    """If quit button is pressed"""
+                    if self.button_pressed(600, 450, 150, 75):
+                        """Set STATE to quit"""
                         STATE = "quit"
-                
-                
-                #if event.type == pygame.QUIT:
-                #    STATE = "quit"
-                
-                # Presentitionals
+                        """Go to next event in event list"""
+                        continue
+                """If in trial state"""
+                if STATE == "trial":
+                    """If left mouse button is pressed"""
+                    if pygame.mouse.get_pressed()[0] == 1:
+                        """Get tile that has been clicked from Board.py"""
+                        clickedRect = self.board.checkMouseClick(pygame.mouse.get_pos())
+                        """If a tile has been clicked"""
+                        if clickedRect is not None:
+                            """If clicked tile was supposed to be clicked"""
+                            if (clickedRect.left, clickedRect.top) == self.sequence[len(self.clickedseq)]:
+                                """Add tile to clickedseq list"""
+                                self.clickedseq.append((clickedRect.left, clickedRect.top))
+                                """If clicked tile was not supposed to be clicked"""
+                            else:
+                                print("WRONG")
+                                """Calculate time used to complete this trial"""
+                                self.time_trial = time() - self.time_start
+                                """Add time to list of trial times"""
+                                self.times.append(self.time_trial)
+                                """Add 2-tuple of WMC and trial time to seqLength_Times_List"""
+                                self.SeqLength_Times_List.append((CURRENT_SEQUENCELENGTH - 1, self.time_trial))
+                                """Add 1 to amount of errors made"""
+                                self.player_errors += 1
+                                """Set STATE to feedback"""
+                                STATE = "feedback"
+                                """Go to next event in event list"""
+                                continue
+                    """If the sequence that has been clicked is the same as the trial sequence"""
+                    if self.clickedseq == self.sequence:
+                        """Set correct_seq to True to indicate the player had entered the correct sequence"""
+                        self.correct_seq = True
+                        """Increase CURRENT_SEQUENCELENGTH by one for the next trial"""
+                        CURRENT_SEQUENCELENGTH += 1
+                        """Reset amount of player errors for the next trial"""
+                        self.player_errors = 0
+                        """Calculate time used to complete this trial"""
+                        self.time_trial = time() - self.time_start
+                        """Add time to list of trial times"""
+                        self.times.append(self.time_trial)
+                        """Add 2-tuple of WMC and trial time to seqLength_Times_List"""
+                        self.SeqLength_Times_List.append((CURRENT_SEQUENCELENGTH - 1, self.time_trial))
+                        """Set STATE to feedback"""
+                        STATE = "feedback"
+                        """Go to the next event in event list"""
+                        # TODO CHECK IF CONTINUE IS NEEDED
+                        continue
+                """If in feedback state"""
+                if STATE == "feedback":
+                    """If next trial button is pressed and player has not made more than one error"""
+                    if self.button_pressed(200, 450, 150, 75) and self.player_errors <= self.ALLOWED_ERRORS:
+                        """If not all trials have been completed"""
+                        if CURRENT_SEQUENCELENGTH <= self.SEQUENCE_LENGTH:
+                            """Initialize new trial with CURRENT_SEQUENCELENGTH"""
+                            self.initializeTrial(CURRENT_SEQUENCELENGTH)
+                            """Set starttime of current trial to current time"""
+                            self.time_start = time()
+                            """Set STATE to trial"""
+                            STATE = "trial"
+                            """Go to the next event in event list"""
+                            # TODO CHECK IF CONTINUE IS NEEDED
+                            continue
+                            """If all trials have been completed"""
+                        else:
+                            """Set STATE to final"""
+                            STATE = "final"
+                            """Go to the next event in event list"""
+                            # TODO CHECK IF CONTINUE IS NEEDED
+                            continue
+                    """If quit button is pressed"""
+                    if self.button_pressed(600, 450, 150, 75):
+                        """Set STATE to final"""
+                        STATE = "final"
+                        """Go to the next event in event list"""
+                        # TODO CHECK IF CONTINUE IS NEEDED
+                        continue
+                """If in final state"""
+                if STATE == "final":
+                    """Save the results of the Game to CSV file"""
+                    self.saveResults(self.SeqLength_Times_List)
+                    """If quit button is pressed"""
+                    if self.button_pressed(400, 450, 200, 100):
+                        """Set STATE to quit"""
+                        STATE = "quit"
+                        """Go to the next event in event list"""
+                        # TODO CHECK IF CONTINUE IS NEEDED
+                        continue
+                """If exit system button is clicked"""
+                if event.type == pygame.QUIT:
+                    """Set STATE to quit"""
+                    STATE = "quit"
+                    """Go to the next event in event list"""
+                    # TODO CHECK IF CONTINUE IS NEEDED
+                    continue
+            """End of for loop"""
+
+            """Presentitionals"""
+
+            """If in welcome state"""
             if STATE == "welcome":
+                """Tell View.py to draw welcome screen"""
                 self.view.draw_welcome()
 
+            """If in trial state"""
             if STATE == "trial":
+                """Tell View.py to draw the trial with current board from Board.py and clicked sequence"""
                 self.view.draw_trial(self.board.getCopy(), self.clickedseq)
-            
+
+            """If in feedback state"""
             if STATE == "feedback":
-                self.view.draw_feedback(self.time_trial, self.correct_seq)
+                """Tell View.py to draw feedback screen with trial time and if clicked sequence was correct"""
+                self.view.draw_feedback(self.time_trial, self.correct_seq, self.player_errors > self.ALLOWED_ERRORS)
 
-
+            """If in final state"""
             if STATE == "final":
-                self.view.draw_final((CURRENT_SEQUENCELENGTH-1), (sum(self.times)/len(self.times)))
-                
+                """Tell View.py to draw final screen with WMC and average trial time"""
+                self.view.draw_final((CURRENT_SEQUENCELENGTH - 1), (sum(self.times) / len(self.times)))
+            """If in quite state"""
             if STATE == "quit":
+                """Close screen"""
                 pygame.display.quit()
+                """Exit pygame"""
                 pygame.quit()
+                """Exit programm"""
                 sys.exit()
-
-
-
-
-
-            self.view.updateDisplay();
-
+            """Update pygame display"""
+            self.view.updateDisplay()
--- View.py
+++ View.py
@@ -1,20 +1,17 @@
 import pygame
-import sys
-from pygame.locals import *
-from time import time, ctime
-from pygame.compat import unichr_, unicode_
+from time import time
 
 ''' This class will be used to control the pygame UI and will be used as the Viewer'''
 
-class View:
 
-    ''' Screen size 1000x800
+class View:
+    """ Screen size 1000x800
         Tile size 99x99
         Tile coordinate is middle of tile
         Bottom Bar Score, Button (Next, Done)
         Tiles flash color to show sequence
         Tiles become color upon clicking and get sequence number
-        Export Hashmap <index, coordinate>  (list of tuples?)   '''
+        Export Hashmap <index, coordinate>  (list of tuples?)   """
 
     # Colors abd screen
     col_white = (250, 250, 250)
@@ -26,7 +23,7 @@ class View:
     col_yellow = (250, 250, 0)
     col_darkblue = (0, 0, 139)
     col_bright_green = (0, 255, 0)
-    col_bright_red = (255, 0 , 0)
+    col_bright_red = (255, 0, 0)
 
     BACKGR_COL = col_white
 
@@ -38,7 +35,6 @@ class View:
         self.screen = screen
         self.SCREEN_SIZE = SCREEN_SIZE
         self.TILE_SIZE = TILE_SIZE
-
 
         self.screen.fill(self.BACKGR_COL)
 
@@ -57,14 +53,10 @@ class View:
         text_rectangle = text_surface.get_rect()
         text_rectangle.center = (self.SCREEN_SIZE[0] / 2.0, self.SCREEN_SIZE[1] / 4.0)
         self.screen.blit(text_surface, text_rectangle)
-        #text_surface = self.font_small.render("Press Spacebar to continue", True, self.col_darkblue, self.BACKGR_COL)
-        #text_rectangle = text_surface.get_rect()
-        #text_rectangle.center = (self.SCREEN_SIZE[0] / 2.0, self.SCREEN_SIZE[1] / 2.0)
-        #self.screen.blit(text_surface, text_rectangle)
-        
+
         self.draw_button("Start", 200, 450, 150, 75, self.col_green, self.col_bright_green, 40)
         self.draw_button("Quit", 600, 450, 150, 75, self.col_red, self.col_bright_red, 40)
-        
+
     def draw_trial(self, tiles, clickedseq):
         for tile in tiles:
             pygame.draw.rect(self.screen, self.col_darkblue, (tile[0], tile[1], self.TILE_SIZE[0], self.TILE_SIZE[1]))
@@ -72,65 +64,74 @@ class View:
         for click in clickedseq:
             pygame.draw.rect(self.screen, self.col_yellow, (click[0], click[1], self.TILE_SIZE[0], self.TILE_SIZE[1]))
 
-
-
     def draw_sequence(self, sequence):
         for seq in sequence:
             time_start = time()
             pygame.draw.rect(self.screen, self.col_yellow, (seq[0], seq[1], self.TILE_SIZE[0], self.TILE_SIZE[1]))
-            self.updateDisplay();
-            bool = True
-            while bool:
-                if time() - time_start == 1:
+            self.updateDisplay()
+            timeBool = True
+            while timeBool:
+                if time() - time_start >= 1:
                     pygame.draw.rect(self.screen, self.col_darkblue,
                                      (seq[0], seq[1], self.TILE_SIZE[0], self.TILE_SIZE[1]))
-                    self.updateDisplay();
-                    bool = False
-    def draw_feedback (self, time, correct):
-        if correct == True:
-            text_surface = self.font_small.render ("You had the correct sequence of blocks", True, self.col_darkblue, self.BACKGR_COL)
-        elif correct == False:
-            text_surface = self.font_small.render ("You had an incorrect sequence of blocks", True, self.col_darkblue, self.BACKGR_COL)
-        
-        
+                    self.updateDisplay()
+                    timeBool = False
+
+    def draw_feedback(self, time, correct, errors_reached):
+        if correct:
+            text_surface = self.font_small.render("You had the correct sequence of blocks", True, self.col_darkblue,
+                                                  self.BACKGR_COL)
+        elif not correct:
+            text_surface = self.font_small.render("You had an incorrect sequence of blocks", True, self.col_darkblue,
+                                                  self.BACKGR_COL)
+
         text_rectangle = text_surface.get_rect()
         text_rectangle.center = (self.SCREEN_SIZE[0] / 2.0, 150)
         self.screen.blit(text_surface, text_rectangle)
 
-        text_surface = self.font_small.render ("Your completion time was: " + str(time) + " seconds", True, self.col_darkblue, self.BACKGR_COL)
+        text_surface = self.font_small.render("Your completion time was: " + str(time) + " seconds", True,
+                                              self.col_darkblue, self.BACKGR_COL)
         text_rectangle = text_surface.get_rect()
         text_rectangle.center = (self.SCREEN_SIZE[0] / 2.0, 300)
         self.screen.blit(text_surface, text_rectangle)
-        
-        self.draw_button ("Next trial", 200, 450, 150, 75, self.col_green, self.col_bright_green, 30)
+
+        if errors_reached:
+            text_surface = self.font_small.render("Maximum amount of errors reached", True,
+                                              self.col_darkblue, self.BACKGR_COL)
+            text_rectangle = text_surface.get_rect()
+            text_rectangle.center = (self.SCREEN_SIZE[0] / 2.0, 400)
+            self.screen.blit(text_surface, text_rectangle)
+
+        if not errors_reached:
+            self.draw_button("Next trial", 200, 450, 150, 75, self.col_green, self.col_bright_green, 30)
         self.draw_button("Quit", 600, 450, 150, 75, self.col_red, self.col_bright_red, 40)
-        
-    def draw_button (self,msg, x, y, w, h, ic, ac, textsize):
+
+    def draw_button(self, msg, x, y, w, h, ic, ac, textsize):
         mouse = pygame.mouse.get_pos()
-        if x+w > mouse [0] > x and y+h > mouse[1] > y:
-            pygame.draw.rect(self.screen, ac, (x,y,w,h))
+        if x + w > mouse[0] > x and y + h > mouse[1] > y:
+            pygame.draw.rect(self.screen, ac, (x, y, w, h))
         else:
-            pygame.draw.rect(self.screen, ic, (x,y,w,h))
-        
+            pygame.draw.rect(self.screen, ic, (x, y, w, h))
+
         smallText = pygame.font.Font("freesansbold.ttf", textsize)
         textSurf, textRect = self.text_objects(msg, smallText)
-        textRect.center = ((x+(w/2)), (y+(h/2)))
+        textRect.center = ((x + (w / 2)), (y + (h / 2)))
         self.screen.blit(textSurf, textRect)
-    
+
     def text_objects(self, text, font):
         textSurface = font.render(text, True, self.col_darkblue)
         return textSurface, textSurface.get_rect()
-    
-    
-    def draw_final(self, WMC = 0, avgTime = 1):
+
+    def draw_final(self, WMC=0, avgTime=1):
         text_surface = self.font_small.render("You have a WMC of " + str(WMC), True, self.col_darkblue, self.BACKGR_COL)
         text_rectangle = text_surface.get_rect()
         text_rectangle.center = (self.SCREEN_SIZE[0] / 2.0, 150)
         self.screen.blit(text_surface, text_rectangle)
 
-        text_surface = self.font_small.render("Your average completion time is " + str(int(avgTime * 1000)) + " ms", True, self.col_darkblue, self.BACKGR_COL)
+        text_surface = self.font_small.render("Your average completion time is " + str(int(avgTime * 1000)) + " ms",
+                                              True, self.col_darkblue, self.BACKGR_COL)
         text_rectangle = text_surface.get_rect()
         text_rectangle.center = (self.SCREEN_SIZE[0] / 2.0, 300)
         self.screen.blit(text_surface, text_rectangle)
-        
+
         self.draw_button("Quit", 400, 450, 200, 100, self.col_red, self.col_bright_red, 45)
