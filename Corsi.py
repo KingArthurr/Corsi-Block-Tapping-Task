a08b7321b9b2aa10c3e75cc0023657c0d0ecf24e
Reinier de Ridder <reinierdr@gmail.com>
2019-01-18 13:52:27 +0100

    RANDOM BUGS BUNNY

--- Corsi.py
+++ Corsi.py
@@ -3,7 +3,4 @@ from Game import Game
 """ Contains main funtion for starting the program."""
 
 """Init Game instance"""
-game = Game()
-
-"""Start main game loop"""
-game.gameLoop()
+game = Game((800, 600), 9, 1)
